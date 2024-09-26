569fbc5feec3f41579f06ca5f1bec9a6490bcf31
Walteriano <walter.8128@gmail.com>
2024-09-26 02:24:29 -0300

    hito1_terminado

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Navbarr from './Navbar';
+import Home from './Home';
+import Footer from './Footer';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbarr />
+        <Home />
+        <Footer />
+      </div>  
+    </>
+  )
+}
+
+export default App
